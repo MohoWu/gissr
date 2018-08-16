280b60e195d0d42cb75b1b4a96669d0518945c51
MohoWu <hao.wu@ricardo.com>
2018-08-16 14:24:49 +0100

    enhance conversion to polygon

--- R/sp_from_data_frame.R
+++ R/sp_from_data_frame.R
@@ -197,7 +197,7 @@ data_frame_to_polygons <- function(df, latitude, longitude, projection, id) {
   }
   
   # Get data part for the SpatialLinesDataFrame
-  data_extras <- dplyr::distinct(df, id)
+  data_extras <- dplyr::distinct(df, id, .keep_all = TRUE)
   
   # Make sp points object
   sp <- data_frame_to_points(df, latitude, longitude, projection)
